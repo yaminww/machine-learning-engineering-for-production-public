ea51ee42e0788908a5c01baf05669c94784ac427
hiudhw <a201223@outlook.com>
2024-09-03 19:42:00 -0600

    testing cidi

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -4,6 +4,7 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
+# update for testing github actions
 
 
 app = FastAPI(title="Predicting Wine Class with batching")
